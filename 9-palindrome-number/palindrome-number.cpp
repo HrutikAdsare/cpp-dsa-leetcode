27e91315e546e5badc66ae840965ab9261408ba8
Hrutik Adsare <148676689+HrutikAdsare@users.noreply.github.com>
2026-07-30 22:58:58 +0530

    Time: 3 ms (44.47%) | Memory: 8.4 MB (92.82%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,13 +4,13 @@ public:
         if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
 
-        int reversedHalf = 0;
+        int reverse = 0;
 
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
+        while (x > reverse) {
+            reverse = reverse * 10 + x % 10;
             x /= 10;
         }
 
-        return x == reversedHalf || x == reversedHalf / 10;
+        return x == reverse || x == reverse / 10;
     }
 };
